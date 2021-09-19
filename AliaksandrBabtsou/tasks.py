305ba50d49e3d5d7c7e62c0b3a005668710e0848
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2021-09-19 15:26:04 +0300

    fiat: finished all tasks in home work 4

--- AliaksandrBabtsou/tasks.py
+++ AliaksandrBabtsou/tasks.py
@@ -1,14 +1,134 @@
+from typing import Dict, List, Set, Tuple
+from functools import reduce
+import string 
+
+
 def replace_double_single_quotes(input_string: str) -> str:
-    def replace_char(ch: str)-> str:
-        if ch =='"':
+    def replace_char(ch: str) -> str:
+        if ch == '"':
             return "'"
-        if ch =="'":
+        if ch == "'":
             return '"'
         return ch
-    return ''.join(list(map(replace_char, [char for char in input_string])))
+    return ''.join(list(map(replace_char, input_string)))
 
 
-
-def is_palindrome(input_string:str)-> bool:
+def is_palindrome(input_string: str) -> bool:
     return True if input_string == input_string[::-1] else False
+
+
+def str_split(input_string: str, sep: str = ' ', maxsplit: int = -1) -> List[str]:
+    result = []
+    idx = 0
+    idx_split = 0
+    if maxsplit == -1:
+        maxsplit = len(input_string)
+    while idx <= len(input_string):
+        if idx_split < maxsplit:
+            idx_sep = input_string.find(sep, idx, len(input_string))
+            if idx_sep == -1:
+                result.append(input_string[idx:])
+                idx_split += 1
+                break
+            word = input_string[idx:idx_sep]
+            if word != '':
+                result.append(word)
+                idx_split += 1
+            idx = idx_sep+1
+        else:
+            result.append(input_string[idx:])
+            break
+    return result
+
+
+def split_by_index(s: str, indexes: List[int]) -> List[str]:
+    result = []
+    idx_start = 0
+    for idx_end in indexes:
+        result.append(s[idx_start:idx_end])
+        idx_start = idx_end
+    print(idx_start)
+    if idx_start < len(s):
+        result.append(s[idx_start:])
+    return result
+
+
+def get_digits(num: int) -> Tuple[int]:
+    return tuple(list([int(s) for s in str(num)]))
+
+
+def get_shortest_word(s: str) -> str:
+    words = s.split(' ')
+    len_max = 0
+    longest_word = ''
+    for word in words:
+        len_word = len(word)
+        if len_word > len_max:
+            len_max = len_word
+            longest_word = word
+    return longest_word
+
+
+def foo(input_list_int: List[int]) -> List[int]:
+    result = []
+    for i in range(len(input_list_int)):
+        list_int = input_list_int[:]
+        list_int.pop(i)
+        result.append(reduce(lambda el_prev, el: el_prev * el, list_int))
+    return result
+
+def get_pairs(lst: List) -> List[Tuple]:
+    if len(lst)<2:
+        return None
+    return list([(lst[i], lst[i+1]) for i in range(len(lst)-1)])
+
+
+def appear_in_all(*s)-> Set[str]:
+    return reduce(lambda prev, next : prev & next,list([set(''.join(iter)) for iter in s]))
+
+
+
+def appear_at_one(*s)-> Set[str]:
+    return reduce(lambda prev, next : prev | next,list([set(''.join(iter)) for iter in s]))
+
+
+
+
+def appear_in_two(*s)-> Set[str]:
+    result = []
+    list_s=list(s)
+    for iter in s:
+        list_s.remove(iter)
+        pair=[iter, ''.join(list_s)]
+        list_s=list(s)
+        result.append(appear_in_all(*pair))
+    return appear_at_one(*result)
+
+def alphabet_not_used(*s)-> Set[str]:
+    return set(string.ascii_lowercase) -set(''.join(s))
+
+
+def generate_squares(num:int)-> dict:    
+    return {i:i**2 for i in range(1,num+1)}
+
+
+
+
+def combine_dicts(*args):
+    def combine_two_dicts(dict1,dict2):
+        result_dict = {**dict1, **dict2}
+        for key, value in result_dict.items():
+            if key in dict1 and key in dict2:
+                    result_dict[key] = value + dict1[key]
+        return result_dict
+    return reduce(lambda prev, next : combine_two_dicts(prev, next),args)
+
+dict_1 = {'a': 100, 'b': 200}
+dict_2 = {'a': 200, 'c': 300}
+dict_3 = {'a': 300, 'd': 100}
+
+combine_dicts(dict_1, dict_2)
+combine_dicts(dict_1, dict_2, dict_3)
+
+
 
--- AliaksandrBabtsou/test_tasks.py
+++ AliaksandrBabtsou/test_tasks.py
@@ -1,14 +1,82 @@
-from tasks import replace_double_single_quotes,is_palindrome
+from tasks import combine_dicts
+from tasks import generate_squares
+from tasks import alphabet_not_used
+from tasks import appear_in_two
+from tasks import appear_at_one
+from tasks import appear_in_all
+from tasks import get_pairs
+from tasks import foo
+from tasks import get_shortest_word
+from tasks import get_digits
+from tasks import split_by_index
+from tasks import replace_double_single_quotes, is_palindrome, str_split
+
 
 def test_task_1():
     assert replace_double_single_quotes('"""""""') == "'''''''"
     assert replace_double_single_quotes("'''''''") == '"""""""'
-    assert replace_double_single_quotes("''\"'EPAM''BEST''\"Company") == "\"\"\'\"EPAM\"\"BEST\"\"\'Company"
+    assert replace_double_single_quotes(
+        "''\"'EPAM''BEST''\"Company") == "\"\"\'\"EPAM\"\"BEST\"\"\'Company"
+
 
 def test_task_2():
-    list_polindrom = ['redivider', 'deified', 'civic', 'radar', 'level', 'rotor', 'kayak', 'reviver', 'racecar', 'madam', 'refer']
+    list_polindrom = ['redivider', 'deified', 'civic', 'radar',
+                      'level', 'rotor', 'kayak', 'reviver', 'racecar', 'madam', 'refer']
     for iter in list_polindrom:
         assert is_palindrome(iter) is True
     list_polindrom = ['EPAM', 'COMPANY', 'BEST', 'FOR', 'WORK']
     for iter in list_polindrom:
         assert is_palindrome(iter) is False
+
+
+def test_task_3():
+    text = 'Love the Python? Love the Python? Love the Python?  the Python?'
+    assert text.split(sep=' ', maxsplit=3) == str_split(
+        text, sep=' ', maxsplit=3)
+    text = 'Love   the   Python?'
+    assert text.split() == str_split(text)
+    text = 'in Company EPAM, Love   the   Python?, in Company EPAM'
+    assert text.split(',') == str_split(text, sep=',')
+
+
+def test_task_4():
+    assert split_by_index("pythoniscool,isn'tit?", [6, 8, 12, 13, 18]) == [
+        "python", "is", "cool", ",", "isn't", "it?"]
+    assert split_by_index("no luck", [42]) == ["no luck"]
+
+def test_task_5():
+    assert get_digits(87178291199)==(8, 7, 1, 7, 8, 2, 9, 1, 1, 9, 9)
+
+def test_task_6():
+    assert get_shortest_word('Python is simple and effective!')=='effective!'
+    assert get_shortest_word('Any pythonista like namespaces a lot.')=='pythonista'
+
+def test_task_7():
+    assert foo([1, 2, 3, 4, 5])==[120, 60, 40, 30, 24]
+    assert foo([3, 2, 1])==[2, 3, 6]
+
+def test_task_8():
+    assert get_pairs([1, 2, 3, 8, 9])==[(1, 2), (2, 3), (3, 8), (8, 9)]
+
+    assert get_pairs(['need', 'to', 'sleep', 'more'])==[('need', 'to'), ('to', 'sleep'), ('sleep', 'more')]
+
+    assert get_pairs([1]) is None
+
+def test_task_9():
+    test_strings = ["hello", "world", "python", ]
+    assert appear_in_all(*test_strings)=={'o'}
+    assert appear_at_one(*test_strings)=={'d', 'e', 'h', 'l', 'n', 'o', 'p', 'r', 't', 'w', 'y'}
+    assert appear_in_two(*test_strings)=={'h', 'l', 'o'}
+    assert alphabet_not_used(*test_strings)=={'a', 'b', 'c', 'f', 'g', 'i', 'j', 'k', 'm', 'q', 's', 'u', 'v', 'x', 'z'}
+
+
+def test_task_10():
+    assert generate_squares(5)=={1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+def test_task_11():
+    dict_1 = {'a': 100, 'b': 200}
+    dict_2 = {'a': 200, 'c': 300}
+    dict_3 = {'a': 300, 'd': 100}
+
+    assert combine_dicts(dict_1, dict_2)=={'a': 300, 'b': 200, 'c': 300}
+    assert combine_dicts(dict_1, dict_2, dict_3)=={'a': 600, 'b': 200, 'c': 300, 'd': 100}
