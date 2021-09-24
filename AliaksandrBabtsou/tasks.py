83c5e8589a8ea0d003d50280a3a089a535c7dba7
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2021-09-24 06:36:59 +0300

    fiat: finish Task  6.4 and tests

--- AliaksandrBabtsou/tasks.py
+++ AliaksandrBabtsou/tasks.py
@@ -1,7 +1,16 @@
+from collections import OrderedDict
+import string
 
 
 __all__ = [
-    'Counter'
+    'Counter',
+    'HistoryDict',
+    'Cipher',
+    'Bird',
+    'FlyingBird',
+    'NonFlyingBird',
+    'SuperBird',
+
 ]
 
 
@@ -11,21 +20,124 @@ class Counter:
         self.__stop = stop
         self.__counter = start
 
-    def setMaxPrice(self, price):
-        self.__maxprice = price
-
     def get(self):
         if self.__stop == -1:
             return self.__counter
         else:
             return self.__counter if self.__counter < self.__stop else self.__stop
- 
+
     def increment(self):
         if self.__stop == -1:
             self.__counter += 1
         else:
-            self.__counter =  self.__counter +1 if self.__counter < self.__stop else self.__stop
+            self.__counter = self.__counter + 1 if self.__counter < self.__stop else self.__stop
             return 'Maximal value is reached.'
+
+
+class HistoryDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__items = []
+
+    def __setitem__(self, key, value):
+        super(HistoryDict, self).__setitem__(key, value)
+        self.__items.append(key)
+
+    def set_value(self, key, value):
+        self.__setitem__(key, value)
+
+    def get_history(self):
+        return self.__items[-10:]
+
+
+class Cipher:
+    def __init__(self, keyword):
+        self._keyword = keyword
+
+        def rem_val_from_str(is_str, val):
+            for char in val:
+                is_str = [value for value in is_str if value != char]
+            return ''.join(is_str)
+        alphabet = string.ascii_uppercase
+        alphabet_cod = self._keyword.upper() + rem_val_from_str(alphabet,
+                                                                self._keyword.upper())
+        alphabet_cod = alphabet_cod + alphabet_cod.lower()+' '
+        self.__encode = {iter[0]: iter[1] for iter in zip(
+            alphabet+alphabet.lower()+' ', alphabet_cod)}
+        self.__decode = {iter[1]: iter[0] for iter in zip(
+            alphabet+alphabet.lower()+' ', alphabet_cod)}
+
+    def encode(self, val):
+        return ''.join([self.__encode[iter] for iter in val])
+
+    def decode(self, val):
+        return ''.join([self.__decode[iter] for iter in val])
+
+
+# import antigravity
+
+
+class Bird:
+    def __init__(self, name):
+        self.name = name
+
+    def fly(self):
+        return f'{self.name} fly'
+
+    def walk(self):
+        return f'{self.name} bird can walk'
+
+class FlyingBird(Bird):
+    def __init__(self, name, *args):
+        if not any(args):
+            self.ration = 'grains'
+        else:
+            self.ration = args
+        super().__init__(name)
+
+    def eat(self):    
+        return f'It eats mostly {self.ration if isinstance(self.ration,str) else ",".join(self.ration)}'
+    def __str__(self) -> str:
+        return f'{self.name} can walk and fly'
+
+class NonFlyingBird(FlyingBird):
+    def __init__(self, name, *args):
+        super().__init__(name, *args)
+    
+    def swim(self):
+        return f'{self.name} bird can swim'
+
+    def __getattribute__(self, name):
+        if name in ['fly']:
+            raise AttributeError(
+                f'{self.name} nobject has no attribute {name!r}')
+        return super(NonFlyingBird, self).__getattribute__(name)
+   
+    def __str__(self) -> str:
+        return f'{self.name} can walk and fly'
+
+    def eat(self):
+        return f'It eats mostly {self.ration if isinstance(self.ration,str) else ",".join(self.ration)}'
+
+
+
+class SuperBird(Bird):
+    def __init__(self, name, *args):
+        if not any(args):
+            self.ration = 'fish'
+        else:
+            self.ration = args
+        super().__init__(name)
+
+    def swim(self):
+        return f'{self.name} swim'
+
+    def __str__(self) -> str:
+        return f'{self.name} can walk, swim and fly'
+    def eat(self):
+        return f'It eats {self.ration if isinstance(self.ration,str) else ",".join(self.ration)}'
+
 
 if __name__ == '__main__':
     pass
--- AliaksandrBabtsou/test.py
+++ AliaksandrBabtsou/test.py
@@ -1,4 +1,5 @@
 from tasks import *
+import pytest
 
 
 def test_task_6_1_1():
@@ -16,4 +17,40 @@ def test_task_6_1_1():
     assert c.get() == 43
     assert c.increment() == 'Maximal value is reached.'
     assert c.get() == 43
-                                                    
+
+
+def test_task_6_1_2():
+    d = HistoryDict({"foo": 42})
+    d.set_value("bar", 43)
+    assert d.get_history()==["bar"]
+
+def test_task_6_1_3():
+    cipher = Cipher("crypto")
+    assert cipher.encode("Hello world")=="Btggj vjmgp"
+    assert cipher.decode("Fjedhc dn atidsn")=="Kojima is genius"  
+
+def test_task_6_1_4_Bird():
+    b = Bird("Any")
+    assert b.walk()=="Any bird can walk"
+
+def test_task_6_1_4_NonFlyingBird():
+    p = NonFlyingBird("Penguin", "fish")
+    assert p.swim()=="Penguin bird can swim"
+
+    assert p.eat()=="It eats mostly fish"
+
+    with pytest.raises(AttributeError) as excinfo:
+        p.fly()
+        assert "Penguin nobject has no attribute 'fly'" in str(excinfo.value)
+    
+
+#AttributeError: 'Penguin' object has no attribute 'fly'
+def test_task_6_1_4_FlyingBird():
+    c = FlyingBird("Canary")
+    assert str(c)=="Canary can walk and fly"
+    assert c.eat()=="It eats mostly grains"
+
+def test_task_6_1_4_SuperBird():
+    s = SuperBird("Gull")
+    assert str(s)=="Gull can walk, swim and fly"
+    assert s.eat()=="It eats fish"                
