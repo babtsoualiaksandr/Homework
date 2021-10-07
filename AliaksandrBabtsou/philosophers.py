9e390a0c0bb88e270030225d1f01a6ffc56a422e
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2021-10-07 18:39:59 +0300

    fiat: Finish philosophers Task

---
+++ AliaksandrBabtsou/philosophers.py
@@ -0,0 +1,33 @@
+import threading
+from contextlib import contextmanager
+_local = threading.local()
+
+
+@contextmanager
+def acquire(*locks):
+    locks = sorted(locks, key=lambda x: id(x))
+    acquired = getattr(_local, 'acquired', [])
+    if acquired and max(id(lock) for lock in acquired) >= id(locks[0]):
+        raise RuntimeError('Lock Order Violation')
+    acquired.extend(locks)
+    _local.acquired = acquired
+    try:
+        for lock in locks:
+            lock.acquire()
+            yield
+    finally:
+        for lock in reversed(locks):
+            lock.release()
+        del acquired[-len(locks):]
+
+def philosopher(left, right):
+    while True:
+        with acquire(left, right):
+            print(threading.currentThread(), 'eating')
+NSTICKS = 5
+chopsticks = [threading.Lock() for n in range(NSTICKS)]
+for n in range(NSTICKS):
+    t = threading.Thread(target=philosopher,
+                         args=(chopsticks[n], chopsticks[(n+1) % NSTICKS]))
+
+t.start()
